47cb5a32166613f0dd5b66c56c56ded1655b64e9
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2016-11-24 10:43:01 +0100

    edit: spec #2

--- specBataille.py
+++ specBataille.py
@@ -5,7 +5,7 @@ from math import abs
 class Position:
 	"""Constructeur de notre classe Pre-condition : les coordonnées appartiennent a la grille, non occupees."""
 	def __init__(self, x, y): # creer_position : INT x INT -> Position -- Instancie une position en stockant son x et son y avec 0 <= x , y <= 20.
-	
+
 
 	def estTouche(self): # estTouche: Position -> boolean -- Renvoie vrai si la position est touche, false sinon.
 
@@ -19,9 +19,9 @@ class Position:
 
 class Bateau:
 
-   
+
     def __init__(self, posDX, posDY, posFX, posFY): # creer_bateeau : INT x INT x INT x INT -> Bateau -- Instancie un bateau.
-        """Constructeur de notre classe 
+        """Constructeur de notre classe
         	Données : posDX = position de depart en x, posDY = position de depart en y, posFX = position d'arrivee en x, posFY = position d'arrivee en y, taille = taille du bateau
 
         	Pre-condition : les coordonnées appartiennent a la grille, non occupees, ne doivent pas representer un bateau en diagonale.
@@ -38,10 +38,9 @@ class Joueur:
 
     def __init__(self): # creer_joueur : -> Joueur -- Instancie un joueur.
 
-
-	def aPerdu(self): # aPerdu : Joueur -> boolean -- Renvoie true si le joueur a perdu, false sinon.
+	def aPerdu(self): # aPerdu : Joueur -> boolean -- Renvoie true si le joueur a perdu (si tous ses bateaux ont coulés), false sinon.
 	def ajoutBateau(self, posDX, posDY, posFX, posFY): # ajoutBateau : Joueur x Bateau -> boolean -- Ajoute un bateau si les cases du nouveau bateau ne sont pas occupe par un bateau deja existant. Renvoie true si succes, false sinon.
-	def tir(self): # tir : Joueur -> INT[2] -- Renvoie un tableau de 2 entier avec 0 <= entier <= 20. 
+	def viser(self): # tir : Joueur -> INT[2] -- Renvoie un tableau de 2 entier avec 0 <= entier <= 20.
 	def getShot(self, x, y): # Joueur x INT x INT -> Int -- Renvoie 0 un bateau est touche, 1 un bateau est en vue, 2 si le tir est a l'eau.
 
 
@@ -50,15 +49,18 @@ def main():
 	j1 = Joueur()
 	j2 = Joueur()
 	joueurs = [j1,j2]
-	taille = [1,2,3,3,4]
-	cpt = 0
-	for i in range(0,2):
+	taille = [1,2,3,3,4] #On fixe les tailles des bateaux à créer
+	cpt = 0 #Compteur pour le nombre de bateau à créer (ici 5)
+
+	'''
+		Saisie des bateaux pour les 2 joueurs
+	'''
+	for i in range(0,2): #Alterner du joueur 1 au joueur2
 		while(cpt != 5):
-			if(taille[cpt] == 1):
-				x1 = -1
+			x1,y1,x2,y2 = -1,-1,-1,-1
+			if(taille[cpt] == 1): #Si la taille est de 1 on saisie une seule coordonnee
 				while( x1 < 0  or x1 > 20):
 					x1 = input("Entrez un x de depart: ")
-				y1 = -1
 				while( y1 < 0  or y1 > 20):
 					y1 = input("Entrez un y de depart: ")
 				x2 = x1
@@ -67,48 +69,37 @@ def main():
 				print("Entrez un bateau de taille : " + taille[cpt])
 				valide = False
 				while(not(valide)):
-					x1 = -1
 					while( x1 < 0  or x1 > 20):
 						x1 = input("Entrez un x de depart: ")
-					y1 = -1
 					while( y1 < 0  or y1 > 20):
 						y1 = input("Entrez un y de depart: ")
-					x2 = -1
 					while( x2 < 0  or x2 > 20):
 						x2 = input("Entrez un x de fin: ")
-					y2 = -1
 					while( y2 < 0  or y2 > 20):
 						y2 = input("Entrez un y de fin: ")
 
-					if(x1 == x2):
-						valide = (abs(y1 - y2) + 1) == taille[cpt]
+					if(x1 == x2):#Verifier que les coordonnees rentrees sont coherentes avec la taille souhaite
+						valide = ((abs(y1 - y2) + 1) == taille[cpt])
 					else if (y1 == y2):
 						valide = (abs(x1 - x2) + 1) == taille[cpt]
-			joueurs[i].ajoutBateau(x1,y1,x2,y2)
+
+					if valide :
+						valide = joueurs[i].ajoutBateau(x1,y1,x2,y2) #si la position n'est pas occupée valide = true
 			cpt = cpt + 1
 
 	fin = False
 	courant = 0
-	next = 1
+	suivant = 1
 	while(not(fin)):
-		tir = joueurs[courant].tir()
-		retour = joueurs[next].getShot(tir[0], tir[1])
+		coordonnee = joueurs[courant].viser()
+		retour = joueurs[suivant].getShot(coordonnee[0], coordonnee[1])
 
 		if(retour == 0):
 			print("Touché !")
-		if(retour == 1):
+		else if(retour == 1):
 			print("En vue !")
-		if(retour == 2):
+		else if(retour == 2):
 			print("A l'eau ")
 
-		for i in xrange(0,2):
-			if(joueurs[i].aPerdu()):
-				fin = True
-
-		tmp = courant
-		courant = next
-		next = tmp
-
-	
-
-
+		fin = joueurs[suivant].aPerdu() #On vérifie que le joueur adverse sur lequel on a tiré a perdu
+		courant, suivant = suivant, courant
