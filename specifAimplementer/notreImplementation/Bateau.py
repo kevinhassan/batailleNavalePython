a9e5dd191d130512496bec223623a52644e5a23d
Mohamed-Iheb FAIZA <mohamed-iheb.faiza@igtp3-mac10.isim.intra>
2016-12-06 11:47:32 +0100

    implementation Bateau

--- specifAimplementer/notreImplementation/Bateau.py
+++ specifAimplementer/notreImplementation/Bateau.py
@@ -1,15 +1,37 @@
 #! /usr/bin/python
 #authors : Thais Aurard, Maxime Soustelle
-
+#implementation : FAIZA Mohamed Iheb
 
 class Bateau:
-	
-	
-	def positionsBateau(self):return
-	#			Bateau --> [int x int](n)
-	def etatPosition(self):return 
-	#			Bateau --> [bool](n)
-	def longueurBateau(self):return
-	#			Bateau --> int
-	def creerBateau(longueur, orientation, coordX, coordY):return 
+    
+    #Remarque : creerBateau c'est le constructeur de bateaux donc on doit change son nom à __init__
 	#			int x str x int x int --> Bateau
+    def __init__(longueur, orientation, coordX, coordY):
+        self.longueur = longueur
+        self.orientation = orientation
+        self.coordX = coordX
+        self.coordY = coordY
+        self.positions = []
+        if orientation == "verticale" :
+            for i in range(0,longueur) :
+                self.positions.append([coordX + i,coordY])
+        else :
+            for i in range(0,longueur) :
+                self.positions.append([coordX,coordY + i])
+        self.etats = []
+        for i in range(0,longueur) :
+            self.etats[i] = False
+        
+    #			Bateau --> [int x int](n)
+	def positionsBateau(self):
+        return self.positions
+	
+    #			Bateau --> [bool](n)
+	def etatPosition(self):
+        return self.etats
+	
+
+    #			Bateau --> int
+    def longueurBateau(self):return
+        return self.longueur
+
