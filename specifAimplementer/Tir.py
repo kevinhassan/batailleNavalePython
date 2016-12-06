8c0893adc4abb3fe947e1cb91057f892209d965b
Kevin HASSAN <kevin.hassan@igtp3-mac09.isim.intra>
2016-12-06 13:00:07 +0100

    remove useless files

--- specifAimplementer/Tir.py
+++ specifAimplementer/Tir.py
@@ -18,8 +18,8 @@ class Tir:
 	def verifierEnVue(self):
 	#			Tir --> bool
         for i in range(0,len(self.joueur.bateaux())) :
-            for l in range(0,len(self.joueur.bateaux()[i].positionsBateau())) :
-                if(self.joueur.bateaux()[i].positionsBateau()[l][0] == coordA and self.joueur.bateaux()[i].positionsBateau()[l][1] == coordB and (not self.joueur.bateaux()[i].etatPosition()[l])) :
+            for l in range(0,len(self.joueur.bateaux()[i].positionBateau())) :
+                if(self.joueur.bateaux()[i].positionBateau()[l][0] == coordA and self.joueur.bateaux()[i].positionBateau()[l][1] == coordB and (not self.joueur.bateaux()[i].etatPosition()[l])) :
                     self.joueur.bateaux()[i].etatPosition()[l] = True
                     isDead = True
                     for a in range(0,len(self.joueur.bateaux()[i].etatPosition())) :
@@ -29,7 +29,7 @@ class Tir:
                         #coule
                     else:
                         #Touche
-                elif(self.joueur.bateaux()[i].positionsBateau()[l][0] == coordA or self.joueur.bateaux()[i].positionsBateau()[l][1] == coordB) :
+                elif(self.joueur.bateaux()[i].positionBateau()[l][0] == coordA or self.joueur.bateaux()[i].positionBateau()[l][1] == coordB) :
                     #En vue
                 else :
                     #A l'eau
